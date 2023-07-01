4867785150c72a887524a07cc2abe97cffd1ec36
keerthiram2612 <keerthiram2612@gmail.com>
2023-07-01 20:47:50 +0530

    creating the education section with close icon

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const btnE1 = document.querySelector(".btn");
 const closeIconE1 = document.querySelector(".close-icon");
 const topContainerE1 = document.querySelector(".top-container");
+const educationE1 =document.querySelector(".education");
+const btn1E1 = document.querySelector(".btn1");
+const closeIconE2 = document.querySelector(".close-icon1");
 
 btnE1.addEventListener("click",()=>{
     topContainerE1.classList.remove("active");
 });
- closeIconE1.addEventListener("click",()=>{
-    topContainerE1.classList.add("active");
- })
+btn1E1.addEventListener("click",()=>{
+    educationE1.classList.remove("active1");
+})
+closeIconE1.addEventListener("click",()=>{
+    educationE1.classList.add("active1");
+})
